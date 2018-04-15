7512943dbf368f8c46be3755edff5539a32a510b
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-04-15 17:27:57 +0100

    more changes

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>Hello {{ name }} This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About',
+    data() {
+      return {
+        name: this.$route.params.name || 'Alex'
+      }
+    },
+  }
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import veeValidate from 'vee-validate';
+import router from './router';
 
+Vue.use(veeValidate);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
